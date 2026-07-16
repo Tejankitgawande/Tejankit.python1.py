507cdb9c4984cc743ac007e1e19656e7a035e3d0
Tejankitgawande <tejankitgawande@gmail.com>
2026-07-16 19:28:47 +0530

    Add files via upload

---
+++ Tejankit.python4.py
@@ -0,0 +1,8 @@
+data =[ -3,4,5,8]
+if len( data)>0 and data [0]>0:
+    print (" first element is positive")
+else:
+    print (" list is empty or first element is negative")
+
+ output:
+          list is empty or first element is negative
