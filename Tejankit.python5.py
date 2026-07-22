fe8c4d9c7ca894adba9ab5a1cf8b2ab7409bda92
Tejankitgawande <tejankitgawande@gmail.com>
2026-07-22 13:44:17 +0530

    Create Tejankit.python5.py

---
+++ Tejankit.python5.py
@@ -0,0 +1,10 @@
+print ( 'Use \n to print a new line')
+print ('\n')
+print ('See what \n  I mean?')
+
+    output:
+            Use 
+              to print a new line
+ 
+       See what
+        I mean?
